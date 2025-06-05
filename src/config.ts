4d53c9fc14ffd46ccb9ae15a034faa50fd9c0826
Cristian Longobardi <cristianlongobardi048@gmail.com>
2025-06-05 15:39:15 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -12,15 +12,30 @@ export const circuits: Circuit[] = [
     id: resistence,
     name: "Circuito Puro Resistivo",
     image: circuitoResistivoImg,
+    calculateCurrent: (settings) => {
+      const x = (settings.peakVoltage / settings.resistence) * Math.cos(settings.phase * Math.PI / 180);
+      const y = (settings.peakVoltage / settings.resistence) * Math.sin(settings.phase * Math.PI / 180);
+      return [x, y];
+    },
   },
   {
     id: capacity,
     name: "Circuito Puro Capacitivo",
     image: circuitoCapacitivoImg,
+    calculateCurrent: (settings) => {
+      const x = (settings.peakVoltage * settings.pulsation * settings.capacity) * Math.cos((settings.phase * Math.PI / 180) + 90);
+      const y = (settings.peakVoltage * settings.pulsation * settings.capacity) * Math.sin((settings.phase * Math.PI / 180) + 90);
+      return [x, y];
+    },
   },
   {
     id: inductance,
     name: "Circuito Puro Induttivo",
     image: circuitoInduttivoImg,
+    calculateCurrent: (settings) => {
+      const x = (settings.peakVoltage / (settings.inductance * settings.pulsation)) * Math.cos((settings.phase * Math.PI / 180) - 90);
+      const y = (settings.peakVoltage / (settings.inductance * settings.pulsation)) * Math.sin((settings.phase * Math.PI / 180) - 90);
+      return [x, y];
+    },
   },
 ];
